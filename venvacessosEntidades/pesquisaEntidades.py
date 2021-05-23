1de2a6e3abe040d94fc52dd09dab71fbd4233e78
richielly <richielly.positivo@gmail.com>
2021-05-23 18:31:17 -0300

    v_0.2.0

---
+++ venvacessosEntidades/pesquisaEntidades.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+import scraping
+import webbrowser
+sistemas = ['esadmin','stp','scf','srh','stm']
+col1, col2, col3 = st.beta_columns(3)
+page_urls_prefeituras = scraping.pesquisa_prefeituras()
+page_urls_camaras = scraping.pesquisa_camaras()
+page_urls_outras = scraping.pesquisa_outras()
+search = st.sidebar.text_input("Pesquisar Entidade")
+
+with col1:
+    st.header('Prefeituras')
+    for entidade, url in page_urls_prefeituras.items():
+        nome = str(entidade)
+        if search.upper() in nome.upper():
+            if st.button(entidade,entidade):
+                webbrowser.open_new_tab('http://'+url)
+with col2:
+    st.header('Câmaras')
+    for camara, url_camara in page_urls_camaras.items():
+        nome_camara = str(camara)
+        if search.upper() in nome_camara.upper():
+            if st.button(camara,camara):
+                webbrowser.open_new_tab('http://'+url_camara)
+with col3:
+    st.header('Outras')
+    for outras, url_outras in page_urls_outras.items():
+        nome_outras = str(outras)
+        if search.upper() in nome_outras.upper():
+            if st.button(outras,outras):
+                webbrowser.open_new_tab('http://'+url_outras)
--- venvacessosEntidades/scraping.py
+++ venvacessosEntidades/scraping.py
@@ -2,8 +2,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 urls=[]
 entidades=[]
-def pagina():
-    url = 'https://sites.google.com/view/entidade/p%C3%A1gina-inicial?authuser=0'
+urls2=[]
+entidades2=[]
+urls3=[]
+entidades3=[]
+def pagina(url = 'https://sites.google.com/view/entidade/p%C3%A1gina-inicial?authuser=0'):
+    #url = 'https://sites.google.com/view/entidade/p%C3%A1gina-inicial?authuser=0'
     response = urlopen(url)
     soup = BeautifulSoup(response, "html.parser")
     all_links = soup.find_all("a")
@@ -19,3 +23,64 @@ def pagina():
 
     entidade_url = dict(zip(entidades, urls))
     return entidade_url
+
+
+def pesquisa_prefeituras(url = 'https://sites.google.com/view/entidade/prefeituras?authuser=0'):
+    response = urlopen(url)
+    soup = BeautifulSoup(response, "html.parser")
+    all_links = soup.find_all("a")
+
+    for link in all_links:
+        entidade = link.get("aria-label")
+        linha = link.get("href")
+        url = str(linha).split("%3A%2F%2F")
+        try:
+            if entidade != None:
+                entidades.append(entidade)
+            urls.append(url[1].split('&sa')[0].replace('%2F','/').replace('%3A',':'))
+        except:
+            "Não encontado"
+    print(len(entidades))
+    print(len(urls))
+    entidade_url = dict(zip(entidades, urls))
+    return entidade_url
+
+def pesquisa_camaras(url = 'https://sites.google.com/view/entidade/camaras?authuser=0'):
+    response = urlopen(url)
+    soup2 = BeautifulSoup(response, "html.parser")
+    all_links2 = soup2.find_all("a")
+
+    for link2 in all_links2:
+        entidade2 = link2.get("aria-label")
+        linha2 = link2.get("href")
+        url2 = str(linha2).split("%3A%2F%2F")
+        try:
+            if entidade2 != None:
+                entidades2.append(entidade2)
+            urls2.append(url2[1].split('&sa')[0].replace('%2F','/').replace('%3A',':'))
+        except:
+            "Não encontado"
+    print(len(entidades2))
+    print(len(urls2))
+    entidade_url2 = dict(zip(entidades2, urls2))
+    return entidade_url2
+
+def pesquisa_outras(url = 'https://sites.google.com/view/entidade/outras?authuser=0'):
+    response = urlopen(url)
+    soup3 = BeautifulSoup(response, "html.parser")
+    all_links3 = soup3.find_all("a")
+
+    for link3 in all_links3:
+        entidade3 = link3.get("aria-label")
+        linha3 = link3.get("href")
+        url3 = str(linha3).split("%3A%2F%2F")
+        try:
+            if entidade3 != None:
+                entidades3.append(entidade3)
+            urls3.append(url3[1].split('&sa')[0].replace('%2F','/').replace('%3A',':'))
+        except:
+            "Não encontado"
+    print(len(entidades3))
+    print(len(urls3))
+    entidade_url3 = dict(zip(entidades3, urls3))
+    return entidade_url3
